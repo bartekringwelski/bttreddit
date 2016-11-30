64c649f8c4602f831fadb29d2f81f7c55f15beac
Bartek Ringwelski <bartekringwelski@MacBook-Pro.local>
2016-11-29 23:45:35 -0800

    got a basic mongoose server running-ish

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.text({ type: 'text/html' }));
 
 
 app.use(express.static('public'));
+
 app.use('/modules', express.static('/node_modules'));
 app.set('view engine', 'ejs');
 port = process.env.PORT || 5001; //
@@ -25,10 +26,11 @@ app.listen(port, function(){
 
 
 
+//used to fetch reddit content from react front-end
 app.get('/populate', function(request, response) {
 
   fs.readFile('/Users/bartekringwelski/Desktop/MKS/1mvp/web/archives/sites/reddit.html', function (err, data) {
-    if(err) { console.error(err) }
+    if(err) { console.error(err) };
     console.log('errrr', err);
     var sitesHTMLdata = Buffer.concat([data]).toString();
     console.log(sitesHTMLdata);
@@ -39,39 +41,62 @@ app.get('/populate', function(request, response) {
 
 
 
-
-
 //mongo database
-// var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test');
-//
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log("connected to freaking mongo db");
-// });
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-// // schema properties
-// var kittySchema = mongoose.Schema({
-//     name: String
-// });
-//
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to freaking mongo db");
+});
+
+// schema properties
+var RedditPageSchema = mongoose.Schema({
+      pageName: String,
+      html: String,
+});
+
+
 // //methods for schema
-// kittySchema.methods.speak = function () {
-//   var greeting = this.name
-//     ? "Meow name is " + this.name
-//     : "I don't have a name";
-//   console.log(greeting);
-// }
-//
-// //intsantiating model
-//
-//
-// var Kitten = mongoose.model('Kitten', kittySchema);
-//
-// var steveCat = new Kitten({ name: 'steve' });
-// console.log(steveCat.name); // 'steve'
-// console.log(steveCat.speak()); // 'steve'
+
+
+//intsantiating model
+
+
+var Page = mongoose.model('Page', RedditPageSchema);
+
+
+//Kitten.find({name: /^fluff/}, (error,results) => console.log(results));
+
+//Kitten.find({ name: /^fluff/ }, callback);
+
+fs.readFile('/Users/bartekringwelski/Desktop/MKS/1mvp/web/archives/sites/loading.html', function (err, data) {
+  if(err) { console.error(err) };
+  var completedHTMLfile = Buffer.concat([data]).toString();
+
+  var loading = new Page({pageName: "loading", html: completedHTMLfile });
+
+  loading.save( (err,loading) => {
+    if (err) {console.log("uh oh");}
+    console.log("saved", loading);
+  });
+});
+
+  console.log("found the contents?");
+  Page.find({pageName: /loading/},(error, object) => {
+    if(error) {
+      console.log(error, "ohooo");
+    } else {
+        console.log(object[0].html);
+      }
+    });
+
+
+//  var query = Kitten.findOne({'name':'loading'})
+//  Page.find({name: /^<div>/}, (error,results) => console.log(results));
+
+//  console.log(query);
 
 
 //routers
